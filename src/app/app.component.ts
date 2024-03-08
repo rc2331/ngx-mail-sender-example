f9221cf76dc305013a545206727f544a6fdd2f53
developerdesigner18 <developer.designer18@gmail.com>
2024-03-08 16:56:40 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormsModule,
+  NgForm,
+  ReactiveFormsModule,
+  Validators,
+  FormBuilder,
+  FormControlName,
+  EmailValidator,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { NgxMailSenderService } from 'ngx-mail-sender';
+import Notiflix from 'notiflix';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  sendemailform: FormGroup | any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private mail: NgxMailSenderService
+  ) {}
+
+  ngOnInit(): void {
+    this.sendemailform = this.formBuilder.group({
+      to: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      text: ['', Validators.required],
+      fromTitle: ['', Validators.required],
+    });
+  }
+
+  sendEmail(formdata: any) {
+    Notiflix.Loading.circle('wait');
+    if (this.sendemailform.valid) {
+      console.log(this.sendemailform.value);
+      this.mail
+        .sendMail({
+          ...this.sendemailform.value,
+          authUser: 'roshanchaudhari.dds@gmail.com',
+          authPass: 'nbma nnyf vnmm hacz',
+          host: 'smtp.gmail.com',
+        })
+        .subscribe(
+          (message) => {
+            Notiflix.Loading.remove();
+            Notiflix.Notify.success('Email Sent Successfully');
+            console.log(message);
+          },
+          (error) => {
+            Notiflix.Loading.remove();
+            Notiflix.Notify.failure('Email not Sent');
+            console.log(error);
+          }
+        );
+    }
+  }
 }
